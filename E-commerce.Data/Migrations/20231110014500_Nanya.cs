b846dd381187dfc9089fb3f3980ffe5ff49fb1f1
Ifunanya <139649435+NanyaSyntax@users.noreply.github.com>
2023-11-10 02:54:27 +0100

    A milestone commit

---
+++ E-commerce.Data/Migrations/20231110014500_Nanya.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookHarbor.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Nanya : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
